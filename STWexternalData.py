be8962fd9cec48514b02ae453934977f6fe7cba1
quicktom <21122850+quicktom@users.noreply.github.com>
2022-12-20 12:28:25 +0100

    Changed.

--- STWexternalData.py
+++ STWexternalData.py
@@ -1,22 +1,21 @@
 
-#
+# Download public data from JPL 
 
 import wget, os
 
 JPLServer = "ftp://naif.jpl.nasa.gov/"
 
 BaseDirectory = "pub/naif/generic_kernels/"
-
-PCKDirectory  = "pck/"
-LSKDirectory  = "lsk/"
+PCKDirectory            = "pck/"
+LSKDirectory            = "lsk/"
 SPKPlanetsDirectory     = "spk/planets/"
-
-DestinationDir          = "./data/naif/"
 
 LSKFile     = "naif0012.tls"
 TPCFile     = "pck00010.tpc"
 BPCFile     = "earth_200101_990628_predict.bpc"
 DE440File   = "de440.bsp"
+
+DestinationDir          = "./data/naif/"
 
 def DownloadFile(externalPath, localPath, fname):
     if not os.path.exists(localPath + fname):
@@ -28,5 +27,3 @@ DownloadFile(JPLServer + BaseDirectory + LSKDirectory,          DestinationDir, 
 DownloadFile(JPLServer + BaseDirectory + PCKDirectory,          DestinationDir, TPCFile)
 DownloadFile(JPLServer + BaseDirectory + PCKDirectory,          DestinationDir, BPCFile)
 DownloadFile(JPLServer + BaseDirectory + SPKPlanetsDirectory,   DestinationDir, DE440File)
-
-
