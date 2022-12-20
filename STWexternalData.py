f4cc1d79f52ecfd27c63be532743b8ee14f48d95
quicktom <21122850+quicktom@users.noreply.github.com>
2022-12-20 12:28:11 +0100

    Changed.

---
+++ STWexternalData.py
@@ -0,0 +1,32 @@
+
+#
+
+import wget, os
+
+JPLServer = "ftp://naif.jpl.nasa.gov/"
+
+BaseDirectory = "pub/naif/generic_kernels/"
+
+PCKDirectory  = "pck/"
+LSKDirectory  = "lsk/"
+SPKPlanetsDirectory     = "spk/planets/"
+
+DestinationDir          = "./data/naif/"
+
+LSKFile     = "naif0012.tls"
+TPCFile     = "pck00010.tpc"
+BPCFile     = "earth_200101_990628_predict.bpc"
+DE440File   = "de440.bsp"
+
+def DownloadFile(externalPath, localPath, fname):
+    if not os.path.exists(localPath + fname):
+        wget.download(externalPath + fname, localPath)
+    else:
+        print(localPath + fname + " exsists already. Skipped")
+
+DownloadFile(JPLServer + BaseDirectory + LSKDirectory,          DestinationDir, LSKFile)
+DownloadFile(JPLServer + BaseDirectory + PCKDirectory,          DestinationDir, TPCFile)
+DownloadFile(JPLServer + BaseDirectory + PCKDirectory,          DestinationDir, BPCFile)
+DownloadFile(JPLServer + BaseDirectory + SPKPlanetsDirectory,   DestinationDir, DE440File)
+
+
