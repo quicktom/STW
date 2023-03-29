0c30fe0c364ac160dee39ddb44005b974b57ed00
quicktom <21122850+quicktom@users.noreply.github.com>
2023-03-29 18:36:04 +0200

    Added find remote tool.

---
+++ FindRemoteString.py
@@ -0,0 +1,15 @@
+import ctypes
+ctypes.CDLL('./hidapi/x64/hidapi.dll')
+
+import hid
+
+s = hid.enumerate()
+for device in s:
+    if device['vendor_id'] == 1452:
+        #print(f"0x{device['vendor_id']:04x}:0x{device['product_id']:04x} {device['product_string']}")
+        if device['usage'] == 5:
+            print(device['path'] )
+
+#h = hid.Device( path=  b'/dev/hidraw5')
+# #print(h)
+
